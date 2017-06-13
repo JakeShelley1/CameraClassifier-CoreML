c790236c89f18dbe7bc87c4115c4d6e7f5322b92
Jake Shelley <jake@Jakes-MacBook-Pro.local>
2017-06-13 18:31:04 -0400

    Initial Commit

---
+++ CameraClassifier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraClassifier
+//
+//  Created by Jake Shelley on 6/13/17.
+//  Copyright © 2017 Jake Shelley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
